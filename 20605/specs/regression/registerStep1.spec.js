6f1f9483505aa1b85f5e58b32ec08a7eef4cc38a
Viktoryia Aliakseyeva <47094751+aliakseyevavikt@users.noreply.github.com>
2020-07-28 12:58:31 -0700

    TLC-318 added tests to the register page 1 (placeholders + correct text) (#80)

--- 20605/specs/regression/registerStep1.spec.js
+++ 20605/specs/regression/registerStep1.spec.js
@@ -41,6 +41,10 @@ describe('REGISTER PAGE STEP 1', function () {
       expect(RegisterPage.lockIcon.isDisplayed()).true;
     });
 
+    it('Terms and agreements checkbox should be displayed', function () {
+      expect(RegisterPage.agreementCheckBox.isSelected()).false;
+    });
+
     it('Terms and agreements link should be displayed', function () {
       expect(RegisterPage.termsAndAgreementsLink.isDisplayed()).true;
     });
@@ -51,6 +55,48 @@ describe('REGISTER PAGE STEP 1', function () {
 
     it('Log in link should be displayed', function () {
       expect(RegisterPage.loginLink.isDisplayed()).true;
+    });
+  });
+
+  describe('ELEMENTS HAVE CORRECT TEXT', function () {
+    it('First Name field placeholder should have correct text', function () {
+      expect(RegisterPage.firstNameInput.getAttribute('placeholder')).eq(registerPageTestResult.firstNamePlaceholder);
+    });
+
+    it('Last Name field placeholder should have correct text', function () {
+      expect(RegisterPage.lastNameInput.getAttribute('placeholder')).eq(registerPageTestResult.lastNamePlaceholder);
+    });
+
+    it('Email field placeholder should have correct text', function () {
+      expect(RegisterPage.email.getAttribute('placeholder')).eq(registerPageTestResult.email);
+    });
+
+    it('Password field placeholder should have correct text', function () {
+      expect(RegisterPage.password.getAttribute('placeholder')).eq(registerPageTestResult.password);
+    });
+
+    it('Agreement label should have correct text', function () {
+      expect(RegisterPage.agreementLabel.getText()).eq(
+        registerPageTestResult.agreementLabel + registerPageTestResult.termsAndAgreementsLink,
+      );
+    });
+
+    it('Terms and agreements link should have correct text', function () {
+      expect(RegisterPage.termsAndAgreementsLink.getText()).eq(registerPageTestResult.termsAndAgreementsLink);
+    });
+
+    it('Register button should have correct text', function () {
+      expect(RegisterPage.registerBtn.getText()).eq(registerPageTestResult.registerBtn);
+    });
+
+    it('Log in prompt label should have correct text', function () {
+      expect(RegisterPage.promptLabel.getText()).eq(
+        registerPageTestResult.loginPromptLabel + ' ' + registerPageTestResult.loginLink + '.',
+      );
+    });
+
+    it('Log in link should have correct text', function () {
+      expect(RegisterPage.loginLink.getText()).eq(registerPageTestResult.loginLink);
     });
   });
 });
