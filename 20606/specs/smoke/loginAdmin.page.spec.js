6b787b39ec173152a031860fb14bb237a40ad47b
SamJulia <skotarjulia@gmail.com>
2020-07-28 00:13:53 -0700

    TLC-302 fixed

--- 20606/specs/smoke/loginAdmin.page.spec.js
+++ 20606/specs/smoke/loginAdmin.page.spec.js
@@ -1,10 +1,9 @@
 import LoginPage from '../../../pageObjects/login.page';
 import ProfilePage from '../../../pageObjects/profile.page';
 import user from '../../../testData/user';
-import wait_time_midium from '../../../testData/waitTimes';
+import wait_time_medium from '../../../testData/waitTimes';
 
-
-describe('SUCCESSFUL LOGIN US ADMIN', () => {
+describe('SUCCESSFUL LOGIN AS ADMIN', () => {
 
   before(() => {
     LoginPage.open();
@@ -12,7 +11,7 @@ describe('SUCCESSFUL LOGIN US ADMIN', () => {
 
   it('Login as role: "ADMIN" (valid input)',  () => {
     LoginPage.validLogin(user.admin.email, user.admin.password);
-    ProfilePage.badge.waitForDisplayed(wait_time_midium);
+    ProfilePage.badge.waitForDisplayed(wait_time_medium);
     expect(ProfilePage.getLoginConfirmation()).eq(`${user.admin.firstName} ${user.admin.lastName}`);
   });
 
