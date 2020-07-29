d924135b03b681c5175e7e6130e07ebca0d2309f
Olga Vergasova <61809448+010V@users.noreply.github.com>
2020-07-28 20:24:16 -0700

    Settings Links selectors updated

--- pageObjects/links.page.js
+++ pageObjects/links.page.js
@@ -1,39 +1,52 @@
 import BasePage from './Base.page';
 
-class linksPage extends BasePage {
-
+class LinksPage extends BasePage {
   get resumeLabel() {
     return $('[for="resume"]');
   }
+
   get resumeField() {
     return $('#resume');
   }
+
   get linkedInLabel() {
     return $('[for="linkedIn"]');
   }
+
   get linkedInField() {
     return $('#linkedIn');
   }
+
   get facebookLabel() {
     return $('[for="facebook"]');
   }
+
   get facebookField() {
     return $('#facebook');
   }
+
   get gitHubLabel() {
     return $('[for="github"]');
   }
+
   get gitHubField() {
     return $('#github');
   }
+
   get codewarsLabel() {
     return $('[for="codewarsUsername"]');
   }
+
   get codewarsField() {
     return $('#codewarsUsername');
   }
+
   get saveLinksBtn() {
     return $('[type="submit"]');
   }
 
+  saveLink() {
+    this.saveLinksBtn.click();
+  }
 }
+export default new LinksPage();
