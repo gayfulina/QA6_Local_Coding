8274a94f59c53622d0f2f56ad90e639a89e1a597
irinag <ig_khv@mail.ru>
2020-07-29 16:08:37 +1000

    adding new and learner login

---
+++ 20601/specs/smoke/login.spec.js
@@ -0,0 +1,27 @@
+import LoginPage from '../../../pageObjects/login.page';
+import ProfilePage from '../../../pageObjects/profile.page';
+import user from '../../../testData/user';
+import WAIT_TIME_MEDIUM from '../../../testData/waitTimes';
+
+describe('LOGIN', () => {
+  it('TC-001 Successful login as an admin', () => {
+    LoginPage.open();
+    LoginPage.validLogin(user.admin.email, user.admin.password);
+    ProfilePage.badge.waitForDisplayed(WAIT_TIME_MEDIUM);
+    expect(ProfilePage.getLoginConfirmation()).eq(user.admin.firstName + ' ' + user.admin.lastName);
+  });
+
+  it('TC-002 Successful login as an new user', () => {
+    LoginPage.open();
+    LoginPage.validLogin(user.new.email, user.new.password);
+    ProfilePage.badge.waitForDisplayed(WAIT_TIME_MEDIUM);
+    expect(ProfilePage.getLoginConfirmation()).eq(user.new.firstName + ' ' + user.new.lastName);
+  });
+
+  it('TC-003 Successful login as an learner', () => {
+    LoginPage.open();
+    LoginPage.validLogin(user.learner.email, user.learner.password);
+    ProfilePage.badge.waitForDisplayed(WAIT_TIME_MEDIUM);
+    expect(ProfilePage.getLoginConfirmation()).eq(user.learner.firstName + ' ' + user.learner.lastName);
+  });
+});
