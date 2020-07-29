7d68eaec2dbc863f35e7bc35c83b5b0cb7ee21d6
Nicolae Odagiu <cyxoi1@yandex.ru>
2020-07-29 05:15:19 +0100

    TLC-253 Create Footer PO (#42)

---
+++ pageObjects/footer.page.js
@@ -0,0 +1,38 @@
+import BasePage from './../pageObjects/Base.page';
+
+class FooterPage extends BasePage {
+  get footer() {
+    return $('[class="mt-auto mb-15rem"]').getText();
+  }
+
+  get contactUs() {
+    return $('//footer/a[@href="/contact"]');
+  }
+
+  get termsOfService() {
+    return $('//footer/a[@href="/terms"]');
+  }
+
+  get privacyPolicy() {
+    return $('//footer/a[@href="/privacy"]');
+  }
+
+  navToContactUs() {
+    this.contactUs.click();
+  }
+
+  navToTermsOfService() {
+    this.termsOfService.click();
+  }
+
+  navToPrivacyPolicy() {
+    this.privacyPolicy.click();
+  }
+
+  open(path) {
+    return super.open('https://stage.localcoding.us/');
+  }
+}
+
+export default new FooterPage();
+
