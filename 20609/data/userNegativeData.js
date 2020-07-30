1e3a3e41bc3ef364f5274d93f077dbdd06476ae8
Lyudmyla Tkachenko <58760093+ludatkach@users.noreply.github.com>
2020-07-30 16:33:28 -0700

    TLC-358 User Login Negative Regression (#134)

--- 20609/data/userNegativeData.js
+++ 20609/data/userNegativeData.js
@@ -2,4 +2,8 @@ const userNegativeData = {
     emailOneLetter: 'a',
     email30Symbols: 'aaaaa@aaaaa.aaaaaaaaaaaaaaaaaa',
     password30Symbols: '123456789012345678901234567890',
+    emailInvalid: 'invalid@email.com',
+    passwordInvalid: 'invalidPassword'
 };
+
+export {userNegativeData};
