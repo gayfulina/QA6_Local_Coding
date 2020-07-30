2a6bc0aeaa6b249407d50b66c8b9c68e04498f54
Marina Labutina <63440386+MarinaLabutina@users.noreply.github.com>
2020-07-30 02:11:52 -0400

    TLC-332 Create function to choose checkbox random in Day report

--- 20601/data/daily.data.js
+++ 20601/data/daily.data.js
@@ -1,6 +1,7 @@
 import faker from 'faker';
 
 const randomData = {
+  checkbox: faker.random.number(11),
   moraleSelectorIndex: faker.random.number(10),
   hoursSelectorIndex: faker.random.number({ min: 11, max: 21 }),
   text: faker.random.words(20),
