18cd60f5bee807fcf5204e1f6fcd09a3849fa2a3
Alina Archangelsky <alina@archangelsky.us>
2020-07-31 10:57:34 -0700

    TLC-384 removed code duplication for login test

--- 20605/specs/smoke/login.js
+++ 20605/specs/smoke/login.js
@@ -2,39 +2,35 @@ import LoginPage from '../../../pageObjects/login.page';
 import ProfilePage from '../../../pageObjects/profile.page';
 import TopMenuPage from '../../../pageObjects/topMenu.page';
 import user from '../../../testData/user';
-import {WAIT_TIME_SHORT} from '../../../testData/waitTimes';
+import { WAIT_TIME_SHORT } from '../../../testData/waitTimes';
 
 describe('LOGIN FUNCTIONALITY', function() {
 
-  before(() => {
+  beforeEach(() => {
     LoginPage.open();
   });
 
   it('should verify that user can login as NEW USER', function() {
-    LoginPage.open();
     LoginPage.login(user.new.email, user.new.password);
-    TopMenuPage.userAvatarName.waitForDisplayed({WAIT_TIME_SHORT});
-    expect((ProfilePage.headerProfile).getText()).eq(user.new.firstName + ' ' + user.new.lastName);
+    TopMenuPage.userAvatarName.waitForDisplayed({ WAIT_TIME_SHORT });
+    expect(ProfilePage.getLoginConfirmation()).eq(user.new.firstName + ' ' + user.new.lastName);
   });
 
   it('should verify that user can login as ADMIN', function() {
-    LoginPage.open();
     LoginPage.login(user.admin.email, user.admin.password);
-    TopMenuPage.userAvatarName.waitForDisplayed({WAIT_TIME_SHORT});
-    expect((ProfilePage.headerProfile).getText()).eq(user.admin.firstName + ' ' + user.admin.lastName);
+    TopMenuPage.userAvatarName.waitForDisplayed({ WAIT_TIME_SHORT });
+    expect(ProfilePage.getLoginConfirmation()).eq(user.admin.firstName + ' ' + user.admin.lastName);
   });
 
   it('should verify that user can login as LEARNER', function() {
-    LoginPage.open();
     LoginPage.login(user.learner.email, user.learner.password);
-    TopMenuPage.userAvatarName.waitForDisplayed({WAIT_TIME_SHORT});
-    expect((ProfilePage.headerProfile).getText()).eq(user.learner.firstName + ' ' + user.learner.lastName);
+    TopMenuPage.userAvatarName.waitForDisplayed({ WAIT_TIME_SHORT });
+    expect(ProfilePage.getLoginConfirmation()).eq(user.learner.firstName + ' ' + user.learner.lastName);
   });
 
   it('should verify that user can login as STUDENT', function() {
-    LoginPage.open();
     LoginPage.login(user.student.email, user.student.password);
-    TopMenuPage.userAvatarName.waitForDisplayed({WAIT_TIME_SHORT});
-    expect((ProfilePage.headerProfile).getText()).eq(user.student.firstName + ' ' + user.student.lastName);
+    TopMenuPage.userAvatarName.waitForDisplayed({ WAIT_TIME_SHORT });
+    expect(ProfilePage.getLoginConfirmation()).eq(user.student.firstName + ' ' + user.student.lastName);
   });
 });
