1bc6402606ba0bcc568cd0f061a6ca57a2253073
Kseniia Sydorenko <63469266+Kseniia855@users.noreply.github.com>
2020-07-31 21:00:59 -0400

    TLC-366 (#135)
    * temp commit
    
    * Check register link on home page

--- 20611/specs/smoke/login.js
+++ 20611/specs/smoke/login.js
@@ -9,6 +9,37 @@ describe('New user registration page', () => {
     LoginPage.open();
   });
 
+  // describe('LOGIN NEW USER', function () {
+  //   it('should Successful login as a new user', () => {
+  //     LoginPage.validLogin(user.new.email, user.new.password);
+  //     ProfilePage.badgeRole.waitForDisplayed(WAIT_TIME_MEDIUM);
+  //     expect(ProfilePage.getLoginConfirmation()).eq(user.new.firstName + ' ' + user.new.lastName);
+  //   });
+  // });
+  //
+  // describe('LOGIN ADMIN', function () {
+  //   it('should Successful login as an Admin', () => {
+  //     LoginPage.validLogin(user.admin.email, user.admin.password);
+  //     ProfilePage.badgeRole.waitForDisplayed(WAIT_TIME_MEDIUM);
+  //     expect(ProfilePage.getLoginConfirmation()).eq(user.admin.firstName + ' ' + user.admin.lastName);
+  //   });
+  // });
+  //
+  // describe('LOGIN LEARNER', function () {
+  //   it('should Successful login as a learner', () => {
+  //     LoginPage.validLogin(user.learner.email, user.learner.password);
+  //     ProfilePage.badgeRole.waitForDisplayed(WAIT_TIME_MEDIUM);
+  //     expect(ProfilePage.getLoginConfirmation()).eq(user.learner.firstName + ' ' + user.learner.lastName);
+  //   });
+  // });
+  //
+  // describe('LOGIN NEW STUDENT', function () {
+  //   it('should Successful login as a student', () => {
+  //     LoginPage.validLogin(user.student.email, user.student.password);
+  //     ProfilePage.badgeRole.waitForDisplayed(WAIT_TIME_MEDIUM);
+  //     expect(ProfilePage.getLoginConfirmation()).eq(user.student.firstName + ' ' + user.student.lastName);
+  //   });
+  // });
   describe('LOGIN NEW USER', function () {
     it('should Successful login as a new user', () => {
       LoginPage.validLogin(user.new.email, user.new.password);
