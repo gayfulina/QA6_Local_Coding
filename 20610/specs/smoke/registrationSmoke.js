e63672f2acd611d496fad62fa6ed5b4561051aba
SamJulia <skotarjulia@gmail.com>
2020-07-31 22:37:02 -0700

    TLC-398 add delete method to registrationSmoke.js

--- 20610/specs/smoke/registrationSmoke.js
+++ 20610/specs/smoke/registrationSmoke.js
@@ -3,12 +3,18 @@ import RegisterStep2Page from '../../../pageObjects/registerStep2.page';
 import expected from '../../../20610/data/expected.json';
 import { newUser } from '../../data/fakeData';
 import ProfilePage from '../../../pageObjects/profile.page';
+import { userDelete } from '../../../helpers/deleteNewUserByAxios'
+
+before(() => {
+  RegisterPage.open();
+});
+
+after('Should delete a user', async () => {
+  const res = await userDelete(newUser.email);
+  expect(res.success).eq(true);
+});
 
 describe('NEW USER REGISTER', () => {
-  before(() => {
-    RegisterPage.open();
-  });
-
   it('UR02 should register user', () => {
     RegisterPage.registerUser(newUser);
     RegisterStep2Page.registerUserStep2(newUser);
