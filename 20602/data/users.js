0426813ed14d3134556b0e0c6d9c9c01d35f3ece
Svetlana Gorbunova <60769408+sviet-ka@users.noreply.github.com>
2020-07-31 22:02:01 -0700

    TLC-326 Created shippingAddress.page.js in pajeObject directory (#88)

--- 20602/data/users.js
+++ 20602/data/users.js
@@ -5,6 +5,14 @@ const user = {
   lastName: faker.name.lastName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
+  shippingStreetAddress: faker.address.streetAddress(),
+  shippingCity: faker.address.city(),
+  shippingPostalCode: faker.address.zipCode(),
+  shippingContactPhone: faker.random().number({min:100000000, max:9999999999}),
+}
+
+const shippingAddressData = {
+
 }
 
 
