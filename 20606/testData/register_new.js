a2b6abdf4374be9b2a00ea89c3e5533ecc83e121
AlexZaletaev <alexzalet@yahoo.com>
2020-08-02 16:38:38 -0700

    TLC-223 changed these files

--- 20606/testData/register_new.js
+++ 20606/testData/register_new.js
@@ -1,13 +1,10 @@
 
 import faker from 'faker';
-
 const firstName = faker.name.firstName();
 const lastName = faker.name.lastName();
 const email = faker.internet.email();
 const password = faker.internet.password();
 const phone = 9165338877;
-  // faker.phone.phoneNumber();
-
 const registerNew = {
   firstName,
   lastName,
@@ -15,6 +12,5 @@ const registerNew = {
   password,
   phone
 };
-
 export {registerNew};
 
