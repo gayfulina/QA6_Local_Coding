8f35c5727b0042a68cdbd5bac5bfcae1f13fe58d
SamJulia <skotarjulia@gmail.com>
2020-08-03 13:08:52 -0700

    TLC-428 add delete method to registrationStep2Smoke.js

--- 20610/specs/smoke/registrationStep2Smoke.js
+++ 20610/specs/smoke/registrationStep2Smoke.js
@@ -3,14 +3,20 @@ import RegisterStep2Page from '../../../pageObjects/registerStep2.page';
 import expected from '../../../20610/data/expected.json';
 import { newUser } from '../../data/fakeData';
 import ProfilePage from '../../../pageObjects/profile.page';
+import { userDelete } from '../../../helpers/deleteNewUserByAxios';
 
-describe('NEW USER REGISTER STEP2', function () {
-  before(function () {
-    RegisterPage.open();
-    RegisterPage.registerUser(newUser);
-  });
+before( 'Should register new user',() => {
+  RegisterPage.open();
+  RegisterPage.registerUser(newUser);
+});
 
-  it('URST2 Should fill data on the step2', function () {
+after('Should delete a user', async () => {
+  const res = await userDelete(newUser.email);
+  expect(res.success).true;
+});
+
+describe('NEW USER REGISTER STEP2',  () => {
+  it('URST2-01 Should fill data on the step2', () => {
     RegisterStep2Page.registerUserStep2(newUser);
     expect(ProfilePage.badgeRole.getText()).eq(expected.userBadges.new);
     expect(ProfilePage.getLoginConfirmation()).eq(`${newUser.firstName} ${newUser.lastName}`);
