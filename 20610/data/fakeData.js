1e5ecd193953cd937511199bc2fb3815ea77e18c
GalinaRuban <galina.ruban11@gmail.com>
2020-08-03 17:34:42 -0400

    contactUsSmoke.js is done, one last expect will be added later on

--- 20610/data/fakeData.js
+++ 20610/data/fakeData.js
@@ -8,5 +8,6 @@ export const newUser = {
   newEmail: faker.internet.email().toLowerCase(),
   password: "QA" + Math.trunc(new Date().getTime() / 10000) + "!$",
   newPassword: "PASV" + Math.trunc(new Date().getTime() / 1000) + "&$",
+  message:  faker.lorem.sentences(faker.random.number({ min: 1, max: 10 })),
   phone: 2157777577
 };
