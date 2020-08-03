3f5a3b913bd0ad876032c03e9469c1a8357d5e80
AleksandraBaidina <aleksandrabaidinas@gmail.com>
2020-08-03 10:59:58 -0500

    Update TLC-243

--- 20608/specs/smoke/login.js
+++ 20608/specs/smoke/login.js
@@ -3,28 +3,29 @@ import LoginPage from './../../../pageObjects/login.page';
 const user = require('./../../../testData/user');
 import ProfilePage from './../../../pageObjects/profile.page';
 
-describe('LOGIN AS ADMIN', () => {
+describe('LOGIN AS ADMIN, LEARNER, STUDENT', () => {
   beforeEach(() => {
     LoginPage.open()
   });
 
   it('should login as Admin', () => {
     LoginPage.login(user.admin.email, user.admin.password);
-    expect(ProfilePage.getLoginConfirmation());
+    expect(ProfilePage.getLoginConfirmation()).eq(user.admin.firstName + ' ' + user.admin.lastName);
     ProfilePage.logout();
   });
 
   it('should login as Learner', () => {
     LoginPage.login(user.learner.email, user.learner.password);
-    expect(ProfilePage.getLoginConfirmation());
+    expect(ProfilePage.getLoginConfirmation()).eq(user.learner.firstName + ' ' + user.learner.lastName);
     ProfilePage.logout();
   });
 
   it('should login as Student', () => {
     LoginPage.login(user.student.email, user.student.password);
-    expect(ProfilePage.getLoginConfirmation());
+    expect(ProfilePage.getLoginConfirmation()).eq(user.student.firstName + ' ' + user.student.lastName);
     ProfilePage.logout();
   });
+
 
 });
 
