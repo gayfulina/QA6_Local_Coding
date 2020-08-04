273cc513b414d723606b5e7058568035f565a31c
Alina-Marukhnenko <Ehovef76>
2020-08-04 13:24:39 -0700

    created tests for linksSettingsFunctional.spec.js

--- 20605/testResult/linksSettings.js
+++ 20605/testResult/linksSettings.js
@@ -6,6 +6,12 @@ const linksSettings = {
   gitHubLabel: 'GitHub profile link',
   codewarsLabel: 'Codewars profile link',
   saveLinkBtn: 'Save',
+  resumeData: 'https://docs.google.com/spreadsheets/d/1UCIVibP8IkrA8pBlxMrIL1L-4PJsRqO6v5SCKq3HboY/edit#gid=1723043706',
+  linkedinData: 'https://www.linkedin.com/in/bogutskii/',
+  facebookData: 'https://www.facebook.com/groups/185538711998736/',
+  gitHubData: 'https://github.com/bogutski',
+  codewarsData: 'https://www.codewars.com/users/Viktor%20Bogutskii',
+
 };
 
 export { linksSettings };
