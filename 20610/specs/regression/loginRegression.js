d469cd74b9a5ccccfecba887a98e795f8c24f90b
hanna.skoropadska <askoropadskaya@gmail.com>
2020-08-03 23:02:47 -0700

    Add login regression tests

--- 20610/specs/regression/loginRegression.js
+++ 20610/specs/regression/loginRegression.js
@@ -6,43 +6,61 @@ import RegisterPage from '../../../pageObjects/register.page';
 import ResetPasswordPage from '../../../pageObjects/reset.password';
 
 before(() => {
-  LoginPage.open();
+    LoginPage.open();
 });
 
 
 describe('Login Regression - All elements are present', () => {
 
   it('LR - 001 h1 should be correct', () => {
-    expect(LoginPage.headerLogin.getText()).eq(expected.loginPageHeader.h1);
+    expect(LoginPage.headerLogin.getText()).eq(expected.loginPage.h1);
   });
 
   it('LR - 002 siteName should be correct', () => {
-     expect(LoginPage.siteName.getText()).eq(expected.loginPageHeader.siteName);
+     expect(LoginPage.siteName.getText()).eq(expected.loginPage.siteName);
     });
 
   it('LR - 003 Should have Email Input Field present on the page', () => {
      expect(LoginPage.inputUsername.isDisplayed()).true;
     });
 
-  it('LR - 004 Should have Password Input Field present on the page', () => {
+    it('LR - 004 Email Input Field should have correct placeholder', function() {
+          expect(LoginPage.inputUsername.getAttribute('placeholder')).eq(expected.loginPage.emailPlaceholder);
+        });
+
+  it('LR - 005 Should have Password Input Field present on the page', () => {
      expect(LoginPage.inputPassword.isDisplayed()).true;
     });
 
-  it('LR - 005 Should have Log in Btn present on the page', () => {
+  it('LR - 006 Password Input Field should have correct placeholder', function() {
+     expect(LoginPage.inputPassword.getAttribute('placeholder')).eq(expected.loginPage.passwordPlaceholder);
+          });
+
+  it('LR - 007 Should have Log in Btn present on the page', () => {
      expect(LoginPage.loginBtn.isDisplayed()).true;
     });
 
-  it('LR - 006 Should "Create one" Link be clickable', () => {
+  it('LR - 008 Should "Create one" Link be clickable', () => {
      LoginPage.registerLink.click();
      expect(RegisterPage.headerRegister.getText()).eq(expected.newUser.h1);
      RegisterPage.loginLink.click();
     });
 
-  it('LR - 007 Should "Reset it" Link be clickable', () => {
+  it('LR - 009 Should "Reset it" Link be clickable', () => {
     LoginPage.resetLink.click();
     expect(ResetPasswordPage.resetHeader.getText()).eq(expected.resetPassword.h1);
     ResetPasswordPage.loginLink.click();
    });
+
+  it('LR - 010 Should have "Sign in with Google" Btn is present on the page', () => {
+       expect(LoginPage.loginGoogleBtn.isDisplayed()).true;
+       expect(LoginPage.loginGoogleBtn.isEnabled()).true;
+   });
+
+  it('LR - 011 Should have "Sign in with Facebook" Btn is present on the page', () => {
+         expect(LoginPage.loginFacebookBtn.isDisplayed()).true;
+         expect(LoginPage.loginFacebookBtn.isEnabled()).true;
+     });
 
 });
 
