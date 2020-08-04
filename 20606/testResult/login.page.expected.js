4539360cd525ef8653dd429a4f376ff71c67b153
Dmitry Tregubov <63436344+4stuntman4@users.noreply.github.com>
2020-08-03 21:59:49 -0700

    TLC-345 Create tests for Login page NEGATIVE (Regression Testing)  (#178)
    TLC-345 Create tests for Login page NEGATIVE (Regression Testing)
    TLC-300 modified tests for Login page role LEARNER POSITIVE (Smoke Testing)

--- 20606/testResult/login.page.expected.js
+++ 20606/testResult/login.page.expected.js
@@ -1,13 +1,14 @@
 const loginPage = {
-  lnkHomePage: "Local Coding",
-  header: "Welcome back!",
-  emailPlaceholder: "Email",
-  passwordPlaceholder: "Password",
-  btnText: "Log in",
-  txtRegister: "Don’t have an account? Create one.",
-  txtReset: "Forgot your password? Reset it.",
-  btnGoogle: "Sign in with Google",
-  btnFacebook: "Sign in with Facebook",
-}
+  lnkHomePage: 'Local Coding',
+  header: 'Welcome back!',
+  emailPlaceholder: 'Email',
+  passwordPlaceholder: 'Password',
+  btnText: 'Log in',
+  txtRegister: 'Don’t have an account? Create one.',
+  txtReset: 'Forgot your password? Reset it.',
+  btnGoogle: 'Sign in with Google',
+  btnFacebook: 'Sign in with Facebook',
+  emailNotValid: "'email' is not a valid email",
+};
 
-export {loginPage};
+export { loginPage };
