7ce0db503f2dae0eece627e332814800d19e5a5b
NadiaDziganchuk <44584434+NadiaDziganchuk@users.noreply.github.com>
2020-08-05 15:01:36 -0700

    ЕС-479 Added test case numeration (loginFunctional and registerStep2Negative) (#238)

--- 20605/specs/regression/loginFunctional.spec.js
+++ 20605/specs/regression/loginFunctional.spec.js
@@ -6,32 +6,32 @@ describe('LOGIN PAGE (FUNCTIONAL)', function () {
     before(() => {
         LoginPage.open();
     });
-    it('should verify that Email field accept any letters (Upper and lower)', function () {
+    it('TC-032 should verify that Email field accept any letters (Upper and lower)', function () {
         LoginPage.login(upperLowerLetters.email, admin.password);
         expect(LoginPage.loginBtn.isDisplayed()).true;
     });
-    it('should verify that Email field accept any numbers', function () {
+    it('TC-033 should verify that Email field accept any numbers', function () {
         browser.refresh();
         LoginPage.login(anyNumbers.email, admin.password);
         expect(LoginPage.loginBtn.isDisplayed()).true;
     });
-    it('should verify that Email field accepts any symbols', function () {
+    it('TC-034 should verify that Email field accepts any symbols', function () {
         browser.refresh();
         LoginPage.login(anySymbols.email, admin.password);
         expect(LoginPage.loginBtn.isDisplayed()).true;
     });
 
-    it('should verify that Password field accept any letters (Upper and lower)', function () {
+    it('TC-035 should verify that Password field accept any letters (Upper and lower)', function () {
         browser.refresh();
         LoginPage.login(admin.email, upperLowerLetters.password);
         expect(LoginPage.loginBtn.isDisplayed()).true;
     });
-    it('should verify that Password field accepts any numbers', function () {
+    it('TC-036 should verify that Password field accepts any numbers', function () {
         browser.refresh();
         LoginPage.login(admin.email, anyNumbers.password);
         expect(LoginPage.loginBtn.isDisplayed()).true;
     });
-    it('should verify that Password field accepts any symbols', function () {
+    it('TC-037 should verify that Password field accepts any symbols', function () {
         browser.refresh();
         LoginPage.login(admin.email, anySymbols.password);
         expect(LoginPage.loginBtn.isDisplayed()).true;
