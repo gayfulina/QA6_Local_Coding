30febdc421dd874356f29f66caec6b65d08588d9
Marina Labutina <63440386+MarinaLabutina@users.noreply.github.com>
2020-08-05 15:57:12 -0400

    TLC-472 Updating of work with constants WAITE_TIME (#233)

--- testData/waitTimes.js
+++ testData/waitTimes.js
@@ -1,5 +1,9 @@
-export const WAIT_TIME_SHORT = 1000;
-export const WAIT_TIME_MEDIUM = 5000;
-export const WAIT_TIME_LONGER = 8000;
-export const WAIT_TIME_LONG = 20000;
-export const WAIT_TIME_LONGEST = 50000;
+const waitTime = {
+  WAIT_TIME_SHORT: 1000,
+  WAIT_TIME_MEDIUM: 5000,
+  WAIT_TIME_LONGER: 8000,
+  WAIT_TIME_LONG: 20000,
+  WAIT_TIME_LONGEST: 50000,
+};
+
+export default waitTime;
