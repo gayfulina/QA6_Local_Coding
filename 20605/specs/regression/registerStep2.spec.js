eac0fac47990adb7ddbcf86f77c4612cf7750282
Alina-Marukhnenko <63672744+Alina-Marukhnenko@users.noreply.github.com>
2020-08-04 19:02:52 -0700

    added TC numbers (#217)

--- 20605/specs/regression/registerStep2.spec.js
+++ 20605/specs/regression/registerStep2.spec.js
@@ -12,61 +12,61 @@ describe('REGISTER PAGE STEP 2', function () {
   });
 
   describe('HEADER', function () {
-    it('header should be displayed', function () {
+    it('TC-077 header should be displayed', function () {
       expect(RegisterStep2Page.headerRegisterStep2.isDisplayed());
     });
 
-    it('header has correct text', function () {
+    it('TC-078 header has correct text', function () {
       expect(RegisterStep2Page.headerRegisterStep2.getText()).eq(
         `Dear ${newUser.firstName} ${newUser.lastName}!\nPlease complete these fields`);
     });
   });
 
   describe('ELEMENTS ARE DISPLAYED', function () {
-    it('Phone field should be displayed', function () {
+    it('TC-083 Phone field should be displayed', function () {
       expect(RegisterStep2Page.phoneNumber.isDisplayed());
     });
 
-    it('Phone field placeholder has correct text', function () {
+    it('TC-086 Phone field placeholder has correct text', function () {
       expect(RegisterStep2Page.phoneNumber.getAttribute('placeholder')).eq(
         registerPageTestResult.phoneNumberPlaceholder);
     });
 
-    it('Phone label is displayed', function () {
+    it('TC-084 Phone label is displayed', function () {
       expect(RegisterStep2Page.phoneLabel.isDisplayed());
     });
 
-    it('Phone label has correct text', function () {
+    it('TC-085 Phone label has correct text', function () {
       expect(RegisterStep2Page.phoneLabel.getText()).eq(
         registerPageTestResult.phoneLabel);
     });
 
-    it('Country field should be displayed', function () {
+    it('TC-079 Country field should be displayed', function () {
       expect(RegisterStep2Page.openDropdown.isDisplayed());
     });
 
-    it('Country label is displayed', function () {
+    it('TC-080 Country label is displayed', function () {
       expect(RegisterStep2Page.countryLabel.isDisplayed());
     });
 
-    it('Country label has correct text', function () {
+    it('TC-081 Country label has correct text', function () {
       expect(RegisterStep2Page.countryLabel.getText()).eq(
         registerPageTestResult.countryLabel);
     });
 
-    it('Submit Btn should be displayed', function () {
+    it('TC-087 Submit Btn should be displayed', function () {
       expect(RegisterStep2Page.SubmitBtn.isDisplayed());
     });
 
-    it('Submit Btn has correct text', function () {
+    it('TC-088 Submit Btn has correct text', function () {
       expect(RegisterStep2Page.SubmitBtn.getText()).eq(registerPageTestResult.submitBtn);
     });
 
-    it('Skip Btn should be displayed', function () {
+    it('TC-089 Skip Btn should be displayed', function () {
       expect(RegisterStep2Page.skipBtn.isDisplayed());
     });
 
-    it('Skip Btn has correct text', function () {
+    it('TC-090 Skip Btn has correct text', function () {
       expect(RegisterStep2Page.skipBtn.getText()).eq(registerPageTestResult.skipBtn);
     });
   });
