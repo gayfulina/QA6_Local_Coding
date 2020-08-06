11a641de90132126f686eea3b199a9c5c30ef0dd
rinsal <notthepeterpig@gmail.com>
2020-08-05 17:51:15 -0700

    refactor expected and registerStep2Route1

--- 20602/specs/regression/registerStep2Route1.spec.js
+++ 20602/specs/regression/registerStep2Route1.spec.js
@@ -12,32 +12,35 @@ before(() => {
   RegisterPage.registerUser(user);
 });
 
+let count = 0;
+let flag = count >= 9 ? '' : '0';
+
 describe('Register step-2 elements', () => {
-  it('TC-008-001 Verify that homepage link is clickable', function () {
+  it(`TC-008-0${flag + (1 + count)} Verify that homepage link is clickable`, function () {
     expect(RegisterStep2Page.homePageLink.isClickable()).true;
   });
 
-  it('TC-008-002 Verify that header is displayed', function () {
+  it(`TC-008-0${flag + (1 + count)} Verify that header is displayed`, function () {
     expect(RegisterStep2Page.headerRegisterStep2.waitForDisplayed(WAIT_TIME_MEDIUM));
   });
 
-  it(`TC-008-003 Verify that header has right text`, function () {
+  it(`TC-008-0${flag + (1 + count)} Verify that header has right text`, function () {
     expect(RegisterStep2Page.headerRegisterStep2.getText()).eq(
       `Dear ${user.firstName} ${user.lastName}!\nPlease complete these fields`,
     );
   });
 
-  it('TC-008-004 Verify that country label is displayed', function () {
+  it(`TC-008-0${flag + (1 + count)} Verify that country label is displayed`, function () {
     expect(RegisterStep2Page.countryLabel.isDisplayed()).true;
   });
 
-  it('TC-008-005 Verify that country label reads "Country"', function () {
+  it(`TC-008-0${flag + (1 + count)} Verify that country label reads "Country"`, function () {
     expect(RegisterStep2Page.countryLabel.getText()).eq(expected.registerStep2Data.countryLabel);
   });
 });
 
 after('Should delete a user', async () => {
-  const res = await userDelete(user.email.toLowerCase());
+  const res = await userDelete(user.email);
   expect(res.success).eq(true);
 });
 
