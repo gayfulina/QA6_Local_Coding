6b15271452a2d6e027c139db5dccdfa23604af4e
Alina-Marukhnenko <Ehovef76>
2020-08-06 11:17:26 -0700

    added TC numbers

--- 20605/specs/regression/topMenuLayoutRoles.spec.js
+++ 20605/specs/regression/topMenuLayoutRoles.spec.js
@@ -9,43 +9,43 @@ describe('MAIN ELEMENTS ARE DISPLAYED for LEARNER', function () {
     topMenuPage.userAvatarName.isDisplayed();
   });
 
-    it('Base Link is displayed', function () {
+    it('TC-235 Base Link is displayed', function () {
       expect(topMenuPage.baseLink.isDisplayed()).false;
     });
 
-    it('Courses Link is displayed', function () {
+    it('TC-236 Courses Link is displayed', function () {
       expect(topMenuPage.coursesLink.isDisplayed());
     });
 
-    it('Cards Link is displayed', function () {
+    it('TC-237 Cards Link is displayed', function () {
       expect(topMenuPage.cardsLink.isDisplayed()).false;
     });
 
-    it('Diary Link is displayed', function () {
+    it('TC-238 Diary Link is displayed', function () {
       expect(topMenuPage.diaryLink.isDisplayed());
     });
 
-    it('Groups Link is displayed', function () {
+    it('TC-239 Groups Link is displayed', function () {
       expect(topMenuPage.groupsLink.isDisplayed()).false;
     });
 
-    it('Users Link is displayed', function () {
+    it('TC-240 Users Link is displayed', function () {
       expect(topMenuPage.usersLink.isDisplayed()).false;
     });
 
-    it('Challenges Link is displayed', function () {
+    it('TC-241 Challenges Link is displayed', function () {
       expect(topMenuPage.challengesLink.isDisplayed()).false;
     });
 
-    it('Telephony Link is displayed', function () {
+    it('TC-242 Telephony Link is displayed', function () {
       expect(topMenuPage.telephonyLink.isDisplayed()).false;
     });
 
-    it('Shop Link is displayed', function () {
+    it('TC-243 Shop Link is displayed', function () {
       expect(topMenuPage.shopLink.isDisplayed()).false;
     });
 
-    it('Orders Link is displayed', function () {
+    it('TC-244 Orders Link is displayed', function () {
       expect(topMenuPage.ordersLink.isDisplayed()).false;
     });
 });
@@ -57,93 +57,91 @@ describe('MAIN ELEMENTS ARE DISPLAYED for STUDENT', function () {
     topMenuPage.userAvatarName.isDisplayed();
   });
 
-  it('Base Link is displayed', function () {
+  it('TC-245 Base Link is displayed', function () {
     expect(topMenuPage.baseLink.isDisplayed()).false;
   });
 
-  it('Courses Link is displayed', function () {
+  it('TC-246 Courses Link is displayed', function () {
     expect(topMenuPage.coursesLink.isDisplayed());
   });
 
-  it('Cards Link is displayed', function () {
+  it('TC-247 Cards Link is displayed', function () {
     expect(topMenuPage.cardsLink.isDisplayed());
   });
 
-  it('Diary Link is displayed', function () {
+  it('TC-248 Diary Link is displayed', function () {
     expect(topMenuPage.diaryLink.isDisplayed());
   });
 
-  it('Groups Link is displayed', function () {
+  it('TC-249 Groups Link is displayed', function () {
     expect(topMenuPage.groupsLink.isDisplayed());
   });
 
-  it('Users Link is displayed', function () {
+  it('TC-250 Users Link is displayed', function () {
     expect(topMenuPage.usersLink.isDisplayed()).false;
   });
 
-  it('Challenges Link is displayed', function () {
+  it('TC-251 Challenges Link is displayed', function () {
     expect(topMenuPage.challengesLink.isDisplayed()).false;
   });
 
-  it('Telephony Link is displayed', function () {
+  it('TC-252 Telephony Link is displayed', function () {
     expect(topMenuPage.telephonyLink.isDisplayed()).false;
   });
 
-  it('Shop Link is displayed', function () {
+  it('TC-253 Shop Link is displayed', function () {
     expect(topMenuPage.shopLink.isDisplayed());
   });
 
-  it('Orders Link is displayed', function () {
+  it('TC-254 Orders Link is displayed', function () {
     expect(topMenuPage.ordersLink.isDisplayed()).false;
   });
 });
 
-describe('MAIN ELEMENTS ARE DISPLAYED for STUDENT', function () {
+describe('MAIN ELEMENTS ARE DISPLAYED for NEW USER', function () {
   before(() => {
     LoginPage.open();
     LoginPage.login(user.new.email, user.new.password);
     topMenuPage.userAvatarName.isDisplayed();
   });
 
-  it('Base Link is displayed', function () {
+  it('TC-225 Base Link is displayed', function () {
     expect(topMenuPage.baseLink.isDisplayed()).false;
   });
 
-  it('Courses Link is displayed', function () {
+  it('TC-226 Courses Link is displayed', function () {
     expect(topMenuPage.coursesLink.isDisplayed()).false;
   });
 
-  it('Cards Link is displayed', function () {
+  it('TC-227 Cards Link is displayed', function () {
     expect(topMenuPage.cardsLink.isDisplayed()).false;
   });
 
-  it('Diary Link is displayed', function () {
+  it('TC-228 Diary Link is displayed', function () {
     expect(topMenuPage.diaryLink.isDisplayed()).false;
   });
 
-  it('Groups Link is displayed', function () {
+  it('TC-229 Groups Link is displayed', function () {
     expect(topMenuPage.groupsLink.isDisplayed()).false;
   });
 
-  it('Users Link is displayed', function () {
+  it('TC-230 Users Link is displayed', function () {
     expect(topMenuPage.usersLink.isDisplayed()).false;
   });
 
-  it('Challenges Link is displayed', function () {
+  it('TC-231 Challenges Link is displayed', function () {
     expect(topMenuPage.challengesLink.isDisplayed()).false;
   });
 
-  it('Telephony Link is displayed', function () {
+  it('TC-232 Telephony Link is displayed', function () {
     expect(topMenuPage.telephonyLink.isDisplayed()).false;
   });
 
-  it('Shop Link is displayed', function () {
+  it('TC-233 Shop Link is displayed', function () {
     expect(topMenuPage.shopLink.isDisplayed()).false;
   });
 
-  it('Orders Link is displayed', function () {
+  it('TC-234 Orders Link is displayed', function () {
     expect(topMenuPage.ordersLink.isDisplayed()).false;
   });
 });
-
-
