314b86d543b348f7c977539002bb36bc2b75fcfd
Edemina <edeminasf@gmail.com>
2020-08-06 13:57:59 -0700

    Add test case numeration (loginLayOut & registerStep1Negative) (#251)

--- 20605/specs/regression/loginLayOut.spec.js
+++ 20605/specs/regression/loginLayOut.spec.js
@@ -1,5 +1,4 @@
 import LoginPage from '../../../pageObjects/login.page';
-import { userAdmin } from '../../testData/login';
 import { loginPage } from '../../testResult/login';
 
 describe('LOGIN PAGE', function () {
@@ -8,57 +7,57 @@ describe('LOGIN PAGE', function () {
   });
 
   describe('HEADER', function () {
-    it('TC- header should be displayed', function () {
+    it('TC-255 header should be displayed', function () {
       expect(LoginPage.headerLogin.isDisplayed()).true;
     });
 
-    it('TC- header should have correct text', function () {
+    it('TC-016 header should have correct text', function () {
       expect(LoginPage.headerLogin.getText()).eq(loginPage.header);
     });
   });
 
   describe('ALL ELEMENTS ARE DISPLAYED', function () {
-    it('should verify that "Mail" icon is displayed', function () {
+    it('TC-017 should verify that "Mail" icon is displayed', function () {
       expect(LoginPage.mailIcon.isDisplayed()).true;
     });
 
-    it('should verify that "Email" input field is displayed', function () {
+    it('TC-018 should verify that "Email" input field is displayed', function () {
       expect(LoginPage.inputUsername.isDisplayed()).true;
     });
 
-    it('should verify that "Email" input field has placeholder "Email"', function () {
+    it('TC-019 should verify that "Email" input field has placeholder "Email"', function () {
       expect(LoginPage.inputUsername.getAttribute('placeholder')).eq(loginPage.emailPlaceholder);
     });
 
-    it('should verify that "Lock" icon is displayed', function () {
+    it('TC-020 should verify that "Lock" icon is displayed', function () {
       expect(LoginPage.lockIcon.isDisplayed()).true;
     });
 
-    it('should verify that "Password" input field is displayed', function () {
+    it('TC-021 should verify that "Password" input field is displayed', function () {
       expect(LoginPage.inputPassword.isDisplayed()).true;
     });
 
-    it('should verify that "Password" input field has placeholder "Email"', function () {
+    it('TC-022 should verify that "Password" input field has placeholder "Email"', function () {
       expect(LoginPage.inputPassword.getAttribute('placeholder')).eq(loginPage.passwordPlaceholder);
     });
 
-    it('should verify that "Log in" button is displayed', function () {
+    it('TC-023 should verify that "Log in" button is displayed', function () {
       expect(LoginPage.loginBtn.isDisplayed()).true;
     });
 
-    it('should verify that register link is displayed', function () {
+    it('TC-024 should verify that register link is displayed', function () {
       expect(LoginPage.registerLink.isDisplayed()).true;
     });
 
-    it('should verify that reset link is displayed', function () {
+    it('TC-025 should verify that reset link is displayed', function () {
       expect(LoginPage.resetLink.isDisplayed()).true;
     });
 
-    it('should verify that GoogleSignIn link is displayed', function () {
+    it('TC-026 should verify that GoogleSignIn link is displayed', function () {
       expect(LoginPage.loginGoogleBtn.isDisplayed()).true;
     });
 
-    it('should verify that FacebookSignIn link is displayed', function () {
+    it('TC-027 should verify that FacebookSignIn link is displayed', function () {
       expect(LoginPage.loginFacebookBtn.isDisplayed()).true;
     });
   });
