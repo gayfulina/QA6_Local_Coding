5f3cb7fa4ae172397bc79c3aea03d16ad9121514
NadiaDziganchuk <44584434+NadiaDziganchuk@users.noreply.github.com>
2020-08-07 11:26:48 -0700

    TLC-499 Update tests LoginFunctional (admin) and waitTime in RegisterStep2Negative (#261)

--- 20605/specs/regression/registerStep2Negative.spec.js
+++ 20605/specs/regression/registerStep2Negative.spec.js
@@ -1,7 +1,7 @@
 import RegisterStep2Page from '../../../pageObjects/registerStep2.page';
 import {negativeRegisterStep2} from '../../testData/register';
 import {errorMessagesRegisterStep2} from '../../testResult/errorMessages';
-import {WAIT_TIME_MEDIUM} from '../../../testData/waitTimes';
+// import waitTime from '../../../testData/waitTimes';
 
 describe('REGISTRATION PAGE 2 (NEGATIVE)', function () {
     beforeEach(() => {
@@ -10,37 +10,43 @@ describe('REGISTRATION PAGE 2 (NEGATIVE)', function () {
 
     it('TC-127 should Phone Number field can not accept incorrect number (min number) of digits and an error message display', function () {
         RegisterStep2Page.phoneNumber.setValue(negativeRegisterStep2.invalidPhoneNumberMin);
-        RegisterStep2Page.errorMessages[0].waitForDisplayed({timeout: WAIT_TIME_MEDIUM});
+        // RegisterStep2Page.errorMessages[0].waitForDisplayed({timeout: waitTime.WAIT_TIME_MEDIUM});
+        browser.waitUntil(() => RegisterStep2Page.errorMessages[0].isDisplayed());
         expect(RegisterStep2Page.errorMessages[0].getText()).eq(errorMessagesRegisterStep2.invalidPhoneNumErrorMessage1);
     });
 
     it('TC-128 should Phone Number field can not accept incorrect number (max numbers) of digits and an error message display', function () {
         RegisterStep2Page.phoneNumber.setValue(negativeRegisterStep2.invalidPhoneNumberMax);
-        RegisterStep2Page.errorMessages[0].waitForDisplayed({timeout: WAIT_TIME_MEDIUM});
+        // RegisterStep2Page.errorMessages[0].waitForDisplayed({timeout: waitTime.WAIT_TIME_MEDIUM});
+        browser.waitUntil(() => RegisterStep2Page.errorMessages[0].isDisplayed());
         expect(RegisterStep2Page.errorMessages[0].getText()).eq(errorMessagesRegisterStep2.invalidPhoneNumErrorMessage1);
     });
 
     it('TC-129 should Phone Number field can not accept symbols and an error message display', function () {
         RegisterStep2Page.phoneNumber.setValue(negativeRegisterStep2.symbolsInvalidPhoneNumber);
-        RegisterStep2Page.errorMessages[0].waitForDisplayed({timeout: WAIT_TIME_MEDIUM});
+        // RegisterStep2Page.errorMessages[0].waitForDisplayed({timeout: waitTime.WAIT_TIME_MEDIUM});
+        browser.waitUntil(() => RegisterStep2Page.errorMessages[0].isDisplayed());
         expect(RegisterStep2Page.errorMessages[0].getText()).eq(errorMessagesRegisterStep2.invalidPhoneNumberErrorMessage2);
     });
 
     it('TC-130 should Phone Number field can not accept Upper letters and an error message display', function () {
         RegisterStep2Page.phoneNumber.setValue(negativeRegisterStep2.lettersUppInvalidPhoneNumber);
-        RegisterStep2Page.errorMessages[0].waitForDisplayed({timeout: WAIT_TIME_MEDIUM});
+        // RegisterStep2Page.errorMessages[0].waitForDisplayed({timeout: waitTime.WAIT_TIME_MEDIUM});
+        browser.waitUntil(() => RegisterStep2Page.errorMessages[0].isDisplayed());
         expect(RegisterStep2Page.errorMessages[0].getText()).eq(errorMessagesRegisterStep2.invalidPhoneNumberErrorMessage2 + '\n' + errorMessagesRegisterStep2.invalidPhoneNumErrorMessage1);
     });
 
     it('TC-131 should Phone Number field can not accept Lower letters and an error message display', function () {
         RegisterStep2Page.phoneNumber.setValue(negativeRegisterStep2.lettersLowerInvalidPhoneNumber);
-        RegisterStep2Page.errorMessages[0].waitForDisplayed({timeout: WAIT_TIME_MEDIUM});
+        // RegisterStep2Page.errorMessages[0].waitForDisplayed({timeout: waitTime.WAIT_TIME_MEDIUM});
+        browser.waitUntil(() => RegisterStep2Page.errorMessages[0].isDisplayed());
         expect(RegisterStep2Page.errorMessages[0].getText()).eq(errorMessagesRegisterStep2.invalidPhoneNumberErrorMessage2);
     });
 
     it('TC-132 should Phone Number field can not accept "Back space" and an error message display', function () {
         RegisterStep2Page.phoneNumber.setValue(negativeRegisterStep2.invalidPhoneNumberBackSpace);
-        RegisterStep2Page.errorMessages[0].waitForDisplayed({timeout: WAIT_TIME_MEDIUM});
+        // RegisterStep2Page.errorMessages[0].waitForDisplayed({timeout: waitTime.WAIT_TIME_MEDIUM});
+        browser.waitUntil(() => RegisterStep2Page.errorMessages[0].isDisplayed());
         expect(RegisterStep2Page.errorMessages[0].getText()).eq(errorMessagesRegisterStep2.invalidPhoneNumberErrorMessage2);
     });
 
