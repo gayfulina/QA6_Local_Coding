a093d3ed6ed69190a1e767b0515d70badc0a547d
AlisaGrinko <63740096+AlisaGrinko@users.noreply.github.com>
2020-08-08 23:10:46 -0400

    TLC 515 import postman, updated tests (#266)

--- 20602/helpers/updateUserRoleByAxios.js
+++ 20602/helpers/updateUserRoleByAxios.js
@@ -1,26 +1,18 @@
 import axios from 'axios';
-import users from '../../testData/user';
 let host = 'https://server-stage.pasv.us';
 
 export const userUpdateRole = async (userEmail, userPassword, role) => {
-  const response = await axios.post(`${host}/user/login`, {
-    email: users.admin.email,
-    password: users.admin.password,
-  });
-  const token = response.data.token;
-
   const resp = await axios.post(`${host}/user/login`, {
     email: `${userEmail}`,
     password: `${userPassword}`,
   });
-  const userId = resp.data.userId;
-  console.log(userId);
+  process.env.RANDOMUSER_ID = resp.data.userId
 
   return axios({
     method: 'patch',
-    url: `${host}/user/${userId}`,
+    url: `${host}/user/${process.env.RANDOMUSER_ID}`,
     headers: {
-      Authorization: token,
+      Authorization: process.env.ADMIN_TOKEN,
     },
     data: {
       roles: [`${role}`],
@@ -30,16 +22,13 @@ export const userUpdateRole = async (userEmail, userPassword, role) => {
     .catch(err => err.response.data);
 };
 
-/* to call the function in your tests After hook should be added similar to below.
-Before you need register user using faker or using yourself user (do not change role of Julia users).
-Example of using userUpdateRole in our folder smoke => settingsPassword.specs.smoke.js
+// to call the function in your tests After hook should be added similar to below.
+// Before you need register user using faker or using yourself user (do not change role of Julia users).
+// Example of using userUpdateRole in our folder smoke => settingsPassword.specs.smoke.js
 
-describe('', () => {
-  it('Should update role a user', async () => { // can be before, after, it
-  const res = await userUpdateRole(userEmail, userPassword, role); //where userEmail is the email and password of the user you want change role, role is a string exp.: 'student'
-    console.log(res)
-  expect(res.success).eq(true);
-  });
-})
-
-*/
+// describe('', () => {
+//   it('Should update role a user', async () => { // can be before, after, it
+//   const res = await userUpdateRole('new@us.com', 'newnew', 'new'); //where userEmail is the email and password of the user you want change role, role is a string exp.: 'student'
+//   expect(res.success).eq(true);
+//   });
+// })
