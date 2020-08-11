c252b419d17151783cba21c21cfc9687bbcd4a79
AlisaGrinko <63740096+AlisaGrinko@users.noreply.github.com>
2020-08-10 21:48:14 -0400

    TLC-521, fixed beforeHook.js (#273)

--- hooks/beforeHook.js
+++ hooks/beforeHook.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const user = require('../testData/user');
+const users = require('../testData/user');
 const host = 'https://server-stage.pasv.us';
 
 async function beforeHook() {
@@ -7,8 +7,8 @@ async function beforeHook() {
     method: 'post',
     url: `${host}/user/login`,
     data: {
-      email: `${user.admin.email}`,
-      password: `${user.admin.password}`,
+      email: `${users.admin.email}`,
+      password: `${users.admin.password}`,
     },
   })
     .then(res => res.data)
