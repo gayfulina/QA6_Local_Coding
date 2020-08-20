7502f2ce23cff57759f53d991ceb326137ba141d
Gennady Gortalov <60904111+gennadyGOR@users.noreply.github.com>
2020-08-20 17:13:21 -0400

    TLC-999 updated login.spec.js (#312)
    Co-authored-by: gennadygortalov <ggortalov@yahoo.com>

--- 20601/specs/smoke/login.spec.js
+++ 20601/specs/smoke/login.spec.js
@@ -10,25 +10,21 @@ describe('LOGIN', () => {
 
   it('TC-01-001 Successful login as an admin', () => {
     LoginPage.validLogin(user.admin.email, user.admin.password);
-    ProfilePage.badgeRole.waitForDisplayed(WAIT_TIME_MEDIUM);
     expect(ProfilePage.getLoginConfirmation()).eq(user.admin.firstName + ' ' + user.admin.lastName);
   });
 
   it('TC-01-002 Successful login as a new user', () => {
-    LoginPage.validLogin(user.new.email, user.new.password);
-    ProfilePage.badgeRole.waitForDisplayed(WAIT_TIME_MEDIUM);
-    expect(ProfilePage.getLoginConfirmation()).eq(user.new.firstName + ' ' + user.new.lastName);
+    LoginPage.validLogin(user.new2.email, user.new2.password);
+    expect(ProfilePage.getLoginConfirmation()).eq(user.new2.firstName + ' ' + user.new2.lastName);
   });
 
   it('TC-01-003 Successful login as a learner', () => {
     LoginPage.validLogin(user.learner.email, user.learner.password);
-    ProfilePage.badgeRole.waitForDisplayed(WAIT_TIME_MEDIUM);
     expect(ProfilePage.getLoginConfirmation()).eq(user.learner.firstName + ' ' + user.learner.lastName);
   });
 
   it('TC-01-004 Successful login as a student', () => {
     LoginPage.validLogin(user.student.email, user.student.password);
-    ProfilePage.badgeRole.waitForDisplayed(WAIT_TIME_MEDIUM);
     expect(ProfilePage.getLoginConfirmation()).eq(user.student.firstName + ' ' + user.student.lastName);
   });
 });
